bcf8ef67be6e952a8ca6a0556913c55b737e3773
AJ Sappenfield <aj.sappenfield@colorado.edu>
2024-01-16 17:25:49 -0700

    Calculator

---
+++ Calculator/operations.cpp
@@ -0,0 +1,8 @@
+//
+//  operations.cpp
+//  Calculator
+//
+//  Created by AJ Sappenfield on 1/16/24.
+//
+
+#include "operations.hpp"
---
+++ Calculator/operations.hpp
@@ -0,0 +1,13 @@
+//
+//  operations.hpp
+//  Calculator
+//
+//  Created by AJ Sappenfield on 1/16/24.
+//
+
+#ifndef operations_hpp
+#define operations_hpp
+
+#include <stdio.h>
+
+#endif /* operations_hpp */
